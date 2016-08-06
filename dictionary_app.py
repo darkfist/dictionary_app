653d7d2b32a57bb5c6bee853c5dbca04d0fe4fbf
Nishant Ranjan <darkfist1998@gmail.com>
2016-08-07 00:08:42 +0530

    Add files via upload

---
+++ dictionary_app.py
@@ -0,0 +1,40 @@
+import re
+import urllib.request
+
+print("Welcome to online Dictionay App.")
+print('Enter any word to get its definition or enter "q" to quit.')
+
+while True:
+	try:
+		keyword = input("\nEnter your word: ")
+		if keyword == "q":
+			break
+		
+		else:
+			url = "http://www.dictionary.com/browse/"
+			url = url + keyword
+			
+			data = urllib.request.urlopen(url).read()
+			data1 = data.decode("utf-8")
+			
+			content = re.search('<meta name="description" content="', data1)
+			start = content.end()
+			end = start + 500
+			content1 = data1[start:end]
+			
+			content = re.search("definition, ", content1)
+			start = content.end()
+			content2 = content1[start:]
+			
+			content = re.search(' See more', content2)
+			end = content.start()
+			content3 = content2[:end]
+			
+			print(content3)
+				
+		
+	except NameError:
+		print('Sorry, The word "' + keyword + '" does not exist.')
+
+	except:
+		print('Sorry, The word "' + keyword + '" does not exist.')
